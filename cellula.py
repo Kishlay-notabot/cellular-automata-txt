9ab8568e7031372165891fa00e22adfcb402704c
kishlay-notabot <kishlay.work1@gmail.com>
2024-07-30 21:03:41 +0530

    watching tut

--- cellula.py
+++ cellula.py
@@ -1,9 +1,14 @@
 # shader for cellular automation
 import pygame 
 import sys
+import moderngl
+import array
 screen = pygame.display.set_mode((800, 800))
 clock = pygame.time.Clock()
+ctx = moderngl.create_context()
+quadb = ctx.buffer(data=array(f'[
 
+]'))
 while True:
     screen.fill((0,0,0))
     for event in pygame.event.get():
