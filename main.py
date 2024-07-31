1ef8db5525bbfa7401ba2b7e8b8e9f12044da0f2
kishlay-notabot <kishlay.work1@gmail.com>
2024-07-31 16:10:17 +0530

    CPU parallelization, init life 60fps

---
+++ main.py
@@ -0,0 +1,255 @@
+import pygame
+from concurrent.futures import ThreadPoolExecutor
+import numpy as np
+
+letter_configs = {
+    'a': [[0, 1, 1, 0],
+          [1, 0, 0, 1],
+          [1, 1, 1, 1],
+          [1, 0, 0, 1],
+          [1, 0, 0, 1]],
+
+    'b': [[1,1,1,0],
+          [1,0,0,1],
+          [1,1,1,0],
+          [1,0,0,1],
+          [1,1,1,0]],
+    'c': [[0,1,1,1],
+          [1,0,0,0],
+          [1,0,0,0],
+          [1,0,0,0],
+          [0,1,1,1]],
+    'd': [[1,1,1,0],
+          [1,0,0,1],
+          [1,0,0,1],
+          [1,0,0,1],
+          [1,1,1,0]],
+    'e': [[1,1,1,1],
+          [1,0,0,0],
+          [1,1,1,0],
+          [1,0,0,0],
+          [1,1,1,1]],
+    'f': [[1,1,1,1],
+          [1,0,0,0],
+          [1,1,1,0],
+          [1,0,0,0],
+          [1,0,0,0]],
+    'g': [[0,1,1,1],
+          [1,0,0,0],
+          [1,0,1,1],
+          [1,0,0,1],
+          [0,1,1,1]],
+    'h': [[1,0,0,1],
+          [1,0,0,1],
+          [1,1,1,1],
+          [1,0,0,1],
+          [1,0,0,1]],
+    'i': [[1,1,1,1],
+          [0,1,0,0],
+          [0,1,0,0],
+          [0,1,0,0],
+          [1,1,1,1]],
+    'j': [[1,1,1,1],
+          [0,0,0,1],
+          [0,0,0,1],
+          [1,0,0,1],
+          [0,1,1,0]],
+    'k': [[1,0,0,1],
+          [1,0,1,0],
+          [1,1,0,0],
+          [1,0,1,0],
+          [1,0,0,1]],
+    'l': [[1,0,0,0],
+          [1,0,0,0],
+          [1,0,0,0],
+          [1,0,0,0],
+          [1,1,1,1]],
+    'm': [[1,0,0,0,1],
+          [1,1,0,1,1],
+          [1,0,1,0,1],
+          [1,0,0,0,1],
+          [1,0,0,0,1]],
+    'n': [[1,0,0,0,1],
+          [1,1,0,0,1],
+          [1,0,1,0,1],
+          [1,0,0,1,1],
+          [1,0,0,0,1]],
+    'o': [[0,1,1,0],
+          [1,0,0,1],
+          [1,0,0,1],
+          [1,0,0,1],
+          [0,1,1,0]],
+    'p': [[1,1,1,0],
+          [1,0,0,1],
+          [1,1,1,0],
+          [1,0,0,0],
+          [1,0,0,0]],
+    'q': [[0,1,1,0],
+          [1,0,0,1],
+          [1,0,0,1],
+          [1,0,1,1],
+          [0,1,1,1]],
+    'r': [[1,1,1,0],
+          [1,0,0,1],
+          [1,1,1,0],
+          [1,0,1,0],
+          [1,0,0,1]],
+    's': [[0,1,1,1],
+          [1,0,0,0],
+          [0,1,1,0],
+          [0,0,0,1],
+          [1,1,1,0]],
+    't': [[1,1,1,1],
+          [0,1,0,0],
+          [0,1,0,0],
+          [0,1,0,0],
+          [0,1,0,0]],
+    'u': [[1,0,0,1],
+          [1,0,0,1],
+          [1,0,0,1],
+          [1,0,0,1],
+          [0,1,1,0]],
+    'v': [[1,0,0,0,1],
+          [1,0,0,0,1],
+          [0,1,0,1,0],
+          [0,1,0,1,0],
+          [0,0,1,0,0]],
+    'w': [[1,0,0,0,1],
+          [1,0,0,0,1],
+          [1,0,1,0,1],
+          [1,1,0,1,1],
+          [1,0,0,0,1]],
+    'x': [[1,0,0,0,1],
+          [0,1,0,1,0],
+          [0,0,1,0,0],
+          [0,1,0,1,0],
+          [1,0,0,0,1]],
+    'y': [[1,0,0,1],
+          [1,0,0,1],
+          [0,1,1,0],
+          [0,0,0,1],
+          [0,0,0,1]],
+    'z': [[1,1,1,1],
+          [0,0,0,1],
+          [0,0,1,0],
+          [0,1,0,0],
+          [1,1,1,1]]
+}
+
+def draw_configuration(surface, config, pos, tile_size):
+    for y, row in enumerate(config):
+        for x, value in enumerate(row):
+            if value == 1:
+                rect = pygame.Rect((pos[0] + x) * tile_size, (pos[1] + y) * tile_size, tile_size, tile_size)
+                pygame.draw.rect(surface, pygame.Color('white'), rect)
+
+def update_board(board, width, height):
+    new_board = np.zeros((height, width), dtype=int)
+    for y in range(height):
+        for x in range(width):
+            # Calculate the number of neighbors
+            neighbors = (
+                board[(y-1) % height, (x-1) % width] +
+                board[(y-1) % height, x] +
+                board[(y-1) % height, (x+1) % width] +
+                board[y, (x-1) % width] +
+                board[y, (x+1) % width] +
+                board[(y+1) % height, (x-1) % width] +
+                board[(y+1) % height, x] +
+                board[(y+1) % height, (x+1) % width]
+            )
+            # Apply Conway's Game of Life rules
+            if board[y, x] == 1:
+                new_board[y, x] = 1 if neighbors == 2 or neighbors == 3 else 0
+            else:
+                new_board[y, x] = 1 if neighbors == 3 else 0
+    return new_board
+
+pygame.init()
+
+res = width, height = 1000, 700
+tile = 10
+w, h = width // tile, height // tile
+fps = 60
+surface = pygame.display.set_mode(res)
+clock = pygame.time.Clock()
+
+# Text input box
+input_box = pygame.Rect(10, height - 40, width - 20, 30)
+font = pygame.font.Font(None, 32)
+text = ''
+color_inactive = pygame.Color('lightskyblue3')
+color_active = pygame.Color('dodgerblue2')
+color = color_inactive
+
+active = False
+
+# Game board
+board = np.zeros((h, w), dtype=int)
+
+# Main loop
+running = True
+simulate = False
+with ThreadPoolExecutor() as executor:
+    while running:
+        surface.fill(pygame.Color('black'))
+
+        # Draw grid lines
+        for x in range(0, width, tile):
+            pygame.draw.line(surface, pygame.Color('dimgray'), (x, 0), (x, height))
+        for y in range(0, height, tile):
+            pygame.draw.line(surface, pygame.Color('dimgray'), (0, y), (width, y))
+
+        # Draw cells
+        for y in range(h):
+            for x in range(w):
+                if board[y, x] == 1:
+                    rect = pygame.Rect(x * tile, y * tile, tile, tile)
+                    pygame.draw.rect(surface, pygame.Color('white'), rect)
+
+        # Render text input box
+        txt_surface = font.render(text, True, color)
+        pygame.draw.rect(surface, color, input_box, 2)
+        surface.blit(txt_surface, (input_box.x + 5, input_box.y + 5))
+
+        # Handle events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if input_box.collidepoint(event.pos):
+                    active = not active
+                    color = color_active if active else color_inactive
+                else:
+                    active = False
+                    color = color_active if active else color_inactive
+            if event.type == pygame.KEYDOWN:
+                if active:
+                    if event.key == pygame.K_RETURN:
+                        # Append letter configurations to the board
+                        current_x = 1
+                        current_y = 1
+                        for letter in text:
+                            if letter == ' ':
+                                current_x += 1
+                            else:
+                                config = letter_configs.get(letter, [])
+                                for y, row in enumerate(config):
+                                    for x, value in enumerate(row):
+                                        board[current_y + y - 1, current_x + x - 1] = value
+                                current_x += len(config[0]) + 1
+                        text = ''
+                        simulate = True
+                    elif event.key == pygame.K_BACKSPACE:
+                        text = text[:-1]
+                    else:
+                        text += event.unicode
+
+        if simulate:
+            future = executor.submit(update_board, board, w, h)
+            board = future.result()
+
+        pygame.display.flip()
+        clock.tick(fps)
+
+pygame.quit()
